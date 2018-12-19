ab8cdaa9cb9de386d1896a9b2d6efaaed92141b4
Kahaan <kahaan.r.patel@gmail.com>
2018-12-19 14:27:32 -0800

    added db

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 const { GraphQLServer } = require("graphql-yoga");
 
 // 1
-const typeDefs = `
-type Query {
-  info: String!
-}
-`;
 
 // 2
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    }
+  },
+
+  Mutation: {
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
+    }
   }
 };
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
