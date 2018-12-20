9622c74f63b860b696f44b2b31ce3096ab40ce75
Kahaan <kahaan.r.patel@gmail.com>
2018-12-19 20:59:07 -0800

    added auth

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { APP_SECRET, getUserId } = require("../utils");
+
 async function signup(parent, args, context, info) {
   const password = await bcrypt.hash(args.password, 10);
   const user = await context.prisma.createUser({ ...args, password });
@@ -24,6 +28,15 @@ async function login(parent, context, args, info) {
     token,
     user
   };
+}
+
+function post(parent, args, context, info) {
+  const userId = getUserId(context);
+  return context.prisma.createLink({
+    url: args.url,
+    description: args.description,
+    postedBy: { connect: { id: userId } }
+  });
 }
 
 module.exports = {
