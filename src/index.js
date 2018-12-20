9a7d75d2b2cbc7637f9404b44037d33c18ca46ed
Kahaan <kahaan.r.patel@gmail.com>
2018-12-19 16:51:58 -0800

    added user model to db

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
 
 // 1
 
@@ -24,6 +25,7 @@ const resolvers = {
 // 3
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
